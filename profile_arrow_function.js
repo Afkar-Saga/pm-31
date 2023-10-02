c12a4f7a508e8fdc5f43fb7afc7a517559345700
JouskaPram <pramudita5800@gmail.com>
2023-10-02 12:22:53 +0700

    init

---
+++ profile_arrow_function.js
@@ -0,0 +1,6 @@
+const displayProfile = (namaDepan, namaBelakang, email) => {
+  console.log("Nama: " + namaDepan + " " + namaBelakang)
+  console.log("Email: " + email)
+}
+
+displayProfile("Afkar", "Sukmawan Ahmad", "afkarsukmawanahmad@gmail.com")
---
+++ profile_reg_function.js
@@ -0,0 +1,6 @@
+function displayProfile(namaDepan, namaBelakang, email) {
+  console.log("Nama: " + namaDepan + " " + namaBelakang)
+  console.log("Email: " + email)
+}
+
+displayProfile("Afkar", "Sukmawan Ahmad", "afkarsukmawanahmad@gmail.com")
